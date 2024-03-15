d2197937047a44447d65b5add8b7fb8990145fa5
rajabhisingh <162866768+rajabhisingh@users.noreply.github.com>
2024-03-16 00:28:49 +0530

    Update Application.java

--- src/main/java/com/abhi/Application.java
+++ src/main/java/com/abhi/Application.java
@@ -8,16 +8,12 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-int a =10;
+	}
 
+	
+		
+	
 
-}
-void m1(){
-//logic
-}
-void m2(){
-int a = 10;
-}
 
 
 
