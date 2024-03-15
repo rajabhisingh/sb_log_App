83d3d9f4d14c4b8f9ee331b52270024e819acfcd
rajabhisingh <abhisingh906092@gmail.com>
2024-03-16 00:17:43 +0530

    add

--- src/main/java/com/abhi/Application.java
+++ src/main/java/com/abhi/Application.java
@@ -8,32 +8,8 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-
-                 int a =10;
-		int b=20;
-		int c=30;
-public void m1(){
-int a = 10;
-System.out.println(a);
-}
-//C-103
-pubic void m3(){
-int c =30;
-System.out.println(c);
-		
-	}
-public void m2(){
-int b=10;
-System.out.println(b);
+int a =10;
 
 }
-public void m4(){
-int d =40;
-}
 
-String str = "Anhf";
-public void add(){
-int c = a+b;
-}
 
-int r = 50;
